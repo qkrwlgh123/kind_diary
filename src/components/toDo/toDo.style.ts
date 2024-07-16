e10fde05ed41d3e0b71a31f6783345d0fb14a450
qkrwlgh123 <jiho1024@naver.com>
2024-07-16 20:15:05 +0900

    목록 스크롤 PC 케이스에만 적용

--- src/components/toDo/toDo.style.ts
+++ src/components/toDo/toDo.style.ts
@@ -9,15 +9,20 @@ const TodoContainer = styled.div`
     padding: 0 15px;
   }
 
-  height: 83vh;
+  @media (min-width: 951px) {
+    height: 83vh;
+  }
 `;
 
 const ObjectListContainer = styled.div`
   gap: 10px;
   display: flex;
   flex-direction: column;
-  height: 100%;
-  overflow-y: scroll;
+
+  @media (min-width: 951px) {
+    height: 100%;
+    overflow-y: scroll;
+  }
 `;
 
 export default { TodoContainer, ObjectListContainer };
