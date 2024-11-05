a57f9d1142ea16b8cb029596abe537febde7cbc4
qkrwlgh123 <jiho1024@naver.com>
2024-11-05 11:29:10 +0900

    기본 색상 테마 다크모드로 변경

--- src/utils/themeUtils.ts
+++ src/utils/themeUtils.ts
@@ -3,12 +3,18 @@ import theme from "../styles/layout/themes";
 /** 로컬스토리지에서 저장된 테마 값을 불러오거나, 없다면 시스템 설정에 따라 초기 테마 값 설정 */
 export const getInitialTheme = () => {
   const savedTheme = localStorage.getItem("themeMode");
+
   if (savedTheme && (savedTheme === "light" || savedTheme === "dark")) {
     return savedTheme as keyof typeof theme;
   }
 
-  const prefersDarkMode = window.matchMedia(
-    "(prefers-color-scheme: dark)"
-  ).matches;
-  return prefersDarkMode ? "dark" : "light";
+  /** 시스템 설정(다크모드) 참조 */
+  // const prefersDarkMode = window.matchMedia(
+  //   "(prefers-color-scheme: dark)"
+  // ).matches;
+
+  // return prefersDarkMode ? "dark" : "light";
+
+  /** 기본 테마를 다크모드로 설정 */
+  return "dark";
 };
